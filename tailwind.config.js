17efa780e4382444336f504b7477dab83e389f74
assiddiq <prihatno96@gmail.com>
2023-04-16 15:03:39 +0800

    init tailwind nunjucks

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["templates/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'kitab': ["Kitab"],
+      }
+    },
+  },
+  plugins: [],
+}
+
